99330c45fc41d38ba4c885c1aeb183d6881a1014
Spicer <spicer.bak@usace.army.mil>
2016-03-11 14:17:13 -0500

    got library imported, created a gridded product and plotted.  now opensource pyngl library needs to be tested against arcpy

--- FRF_natneighbor.py
+++ FRF_natneighbor.py
@@ -4,40 +4,73 @@ This projects is created to test out the creation of a tool for the natural neig
 
 assumes
 """
-import scipy.interpolate as interp
+
 import pytess as pt
 import numpy as np
 import csv
-import arcpy
-
-
+#import arcpy
+try:
+    from PyNGL import Ngl as ngl
+except ImportError:
+    raise ImportError("download the PyNGL library from https://www.pyngl.ucar.edu/Download/ place in site packages of python distro")
 # path to file, fed to tool
-fname =  'data/FRF_20151014_subset.csv'
-header = 1  # length of header lines, place to start finding data
-
-
-# import file with given path above
-#lines = np.genfromtxt(fname, delimiter=',', dtype=None)
-raw_x = []
-raw_y = []
-raw_z = []
-with open(fname, 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        raw_x.append(row[0])
-        raw_y.append(row[1])
-        raw_z.append(row[2])
-# initializing values to make strictly nubmers, imported as strings
-num_x = np.zeros(len(raw_x) - header)
-num_y = np.zeros(len(raw_y) - header)
-num_z = np.zeros(len(raw_z) - header)
-tup=[]
-# making strings into floating point numbers
-for ii in range(header,len(raw_x)):
-    num_x[ii-1] = float(raw_x[ii])
-    num_y[ii-1] = float(raw_y[ii])
-    num_z[ii-1] = float(raw_z[ii])
-    tup.append((float(raw_x[ii]),float(raw_y[ii])))  #, float(raw_z[ii])))
+fname =  'data/FRF_20140930_1093_FRF_NAVD88_LARC_GPS_UTC_v20151127.csv'
+header = 0  # length of header lines, place to start finding data
+def frf_grid_product(fname_in, dxdy=5, header=0, **kwargs):
+    """
+    This function will create a gridded product using the FRF coordinates.  It 
+    is designed to currently flow into the standard work flow of the FRF geomorphology
+    workunits work flow currently in place.  Designed to replace the kriging algorithm
+    currently used to create the gridded product.  
+    the function relies on the PyNGL library created by the National Center for Atmospheric
+    Resarch, Supported by NSF.  the library is available here: https://www.pyngl.ucar.edu/Download/
+    once the package is downloaded, place the PyNGL folder into .../X/lib/site-packages/ folder
+    where .../X is your python distribution folder.  eg /home/user/anaconda2/lib/python2.7/site-packages
+    
+    This script was created by Spicer Bak at USACE CHL FRF 
+    contact: spicer.bak@usace.army.mil
+    
+    ::INPUT::
+    :param fname_in: is a csv file formatted as standard frf transect data
+    :param dxdy:  This is the output grid size in meters, default is 5 meters
+            if dxdy = False, then unique x and y grid cells can be defined 
+            using kwargs of dx and dy 
+    :param header: this is the length of file header to be skipped upon csv import
+    
+    :return:
+    
+    """
+    xmax = 950
+    xmin = 50
+    ymax = 1100
+    ymin = -100
+    if dxdy == False:
+        dx = kwargs['dx']
+        dy = kwargs['dy']
+    else:
+        dx = dy = dxdy    
+    # import file with given path above
+    #lines = np.genfromtxt(fname, delimiter=',', dtype=None)
+    raw_x = []
+    raw_y = []
+    raw_z = []
+    with open(fname, 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            raw_x.append(row[7])  # x in string format
+            raw_y.append(row[8])  # y in string format
+            raw_z.append(row[9])  # z in string format
+    # initializing values to make strictly nubmers, imported as strings
+    num_x = np.zeros(len(raw_x) - header)
+    num_y = np.zeros(len(raw_y) - header)
+    num_z = np.zeros(len(raw_z) - header)
+    tup=[]
+    # making strings into floating point numbers
+    for ii in range(header,len(raw_x)):
+        num_x[ii-1] = float(raw_x[ii])
+        num_y[ii-1] = float(raw_y[ii])
+        num_z[ii-1] = float(raw_z[ii])
+        tup.append((float(raw_x[ii]),float(raw_y[ii])))  #, float(raw_z[ii])))
 
 #
 ## do some data checks to ensure proper formatting being done
@@ -49,42 +82,41 @@ for ii in range(header,len(raw_x)):
 #
 ##
 #
+    xgrid = np.arange(np.min(num_x), np.max(num_x), dx) # array of y coords
+    ygrid = np.arange(np.min(num_y), np.max(num_y), dy)  # array of x coords
+    product = ngl.natgrid(num_x, num_y, num_z, xgrid, ygrid)
+   
 
 
-
-# make grid mesh
-dx = 5 # output grid resolution in x
-dy = 5 # output grid resolution in y
-xgrid = np.arange(np.min(num_x), np.max(num_x), dx)
-ygrid = np.arange(min(num_y), max(num_y), dy)
-xx, yy = np.meshgrid(xgrid, ygrid)
-
-ff = interp.interp2d(xx, yy, num_z, kind = 'cubic')
-
-
-vorpolys = pt.voronoi(tup)
-
-from matplotlib import pyplot as plt
-for ii in range(0, np.shape(vorpolys)[0]):
-    if vorpolys[ii][0] != None:
-        #plt.plot(vorpolys[ii][0], 'rd')
-        for pp in range(0, np.shape(vorpolys[ii][1])[0]):
-            if pp == np.shape(vorpolys[ii][1])[0]-1:
-                xs = [vorpolys[ii][1][pp][0], vorpolys[ii][1][0][0]]
-                ys = [vorpolys[ii][1][pp][1], vorpolys[ii][1][0][1]]
-            else:
-                xs = [vorpolys[ii][1][pp][0], vorpolys[ii][1][pp+1][0]]
-                ys = [vorpolys[ii][1][pp][1], vorpolys[ii][1][pp+1][1]]
-            #print pp, ii
-            plt.plot(xs, ys, 'k-')
-plt.figure(figsize=(5,5), edgecolor='red')
-plt.ylim([200, 1500])
-plt.xlim([-200, 1100])
-plt.title('example Veroni Tesselation of FRF survey data')
-plt.xlabel('xshore loc')
-
-
-
+def creat_vorpoly(tup):
+    """
+    This function created voroni polygons based on a scattered data set
+    it relys on the pytess library which can be pip installed eg pip install pytess
+    tup is a list of tuples of the scatter data [(x_i,y_i)...,(x_i+1, y_i+1)] where
+    i is the point number see help pt.voroni
+    """     
+     vorpolys = pt.voronoi(tup)
+        
+        from matplotlib import pyplot as plt
+        for ii in range(0, np.shape(vorpolys)[0]):
+        if vorpolys[ii][0] != None:
+            #plt.plot(vorpolys[ii][0], 'rd')
+            for pp in range(0, np.shape(vorpolys[ii][1])[0]):
+                if pp == np.shape(vorpolys[ii][1])[0]-1:
+                    xs = [vorpolys[ii][1][pp][0], vorpolys[ii][1][0][0]]
+                    ys = [vorpolys[ii][1][pp][1], vorpolys[ii][1][0][1]]
+                else:
+                    xs = [vorpolys[ii][1][pp][0], vorpolys[ii][1][pp+1][0]]
+                    ys = [vorpolys[ii][1][pp][1], vorpolys[ii][1][pp+1][1]]
+                #print pp, ii
+                plt.plot(xs, ys, 'k-')
+        plt.figure(figsize=(5,5), edgecolor='red')
+        plt.ylim([200, 1500])
+        plt.xlim([-200, 1100])
+        plt.title('example Veroni Tesselation of FRF survey data')
+        plt.xlabel('xshore loc')
+    
+    
 
 
 
