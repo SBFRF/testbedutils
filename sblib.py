3317fe22618a5dcf078dbb925dc14a1678d4dcf6
spike <spicer.bak@usace.army.mil>
2019-07-08 10:12:31 -0400

    added mod to tarfile maker

--- sblib.py
+++ sblib.py
@@ -353,7 +353,7 @@ def myTarMaker(tarOutFile, fileList, **kwargs):
     removeFiles = kwargs.get('removeFiles', True)
     with tarfile.open(tarOutFile, compressionString) as tar:
         for fileName in fileList:
-            tar.add(fileName)
+            tar.add(os.path.split(fileName)[-1])
     if removeFiles:
         [os.remove(ff) for ff in fileList]
 
